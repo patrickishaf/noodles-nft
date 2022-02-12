979e5f0049bb4b1547a19c6e1b977afd6494cf5d
patrickishaf <onumdev@gmail.com>
2022-02-12 17:00:49 +0100

    add the contract address to the repo

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,8 +1,9 @@
 const contract = require("../artifacts/contracts/Noodles.sol/Noodles.json");
 require('dotenv').config();
 const API_URL = process.env.API_URL;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const { createAlchemyWeb3 } = require('@alc/alchemy-web3');
 const web3 = createAlchemyWeb3(API_URL);
 
 
-
+const nftContract = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS);
